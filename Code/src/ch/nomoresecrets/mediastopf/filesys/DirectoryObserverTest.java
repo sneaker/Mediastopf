bd11dae25b29648068ee02b07101ffa49dc58bae
martin <mschwab@hsr.ch>
2009-03-23 18:05:03 +0100

    kleine Änderung, aufräumen

--- Code/src/ch/nomoresecrets/mediastopf/filesys/DirectoryObserverTest.java
+++ Code/src/ch/nomoresecrets/mediastopf/filesys/DirectoryObserverTest.java
@@ -10,9 +10,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DirectoryObserverTest {

d8c174df3d28fb43dda6a5a5a6c6aae12254ac3f
marco <marco.birchler@brocit.ch>
2009-03-23 22:39:06 +0100

    Signed-off-by: marco <marco.birchler@brocit.ch>

--- Code/src/ch/nomoresecrets/mediastopf/server/Server.java
+++ Code/src/ch/nomoresecrets/mediastopf/server/Server.java
@@ -12,22 +12,10 @@ import ch.nomoresecrets.mediastopf.domain.*;
 public class Server {
 
 	public Server(int port) {
-		
-		loadData();
 		NetworkServer testserver = new NetworkServer(port);
 		testserver.run();
 		
 	}
 	
-	private void loadData() {
-		//Get data from DB direct via domain object and activerecord
-		String sql = "select * from Auftrag";
-		List<Order> lp = ActiveRecordManager.getObjectList(sql, Order.class);
-		for (Order name: lp) System.out.println(name.toString());
-
-		//Get data from DB via adapter class
-		lp = DbAdapter.getOrderList();
-		for (Order name: lp) System.out.println(name.toString());
-		
-	}
+	
 }

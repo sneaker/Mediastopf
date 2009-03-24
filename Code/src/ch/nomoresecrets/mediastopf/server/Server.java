4008ac6685f8d39105c5b730f3d29c2b94dfefd4
martin <mschwab@hsr.ch>
2009-03-24 15:45:08 +0100

    build failed, removed bad code. missing files in db and domain?

--- Code/src/ch/nomoresecrets/mediastopf/server/Server.java
+++ Code/src/ch/nomoresecrets/mediastopf/server/Server.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 
 import ch.nomoresecrets.mediastopf.server.networking.NetworkServer;
-import ch.nomoresecrets.mediastopf.database.*;
-import ch.nomoresecrets.mediastopf.domain.*;
+//import ch.nomoresecrets.mediastopf.database.*;
+//import ch.nomoresecrets.mediastopf.domain.*;
 
 
 
@@ -21,12 +21,12 @@ public class Server {
 	private void loadData() {
 		//Get data from DB direct via domain object and activerecord
 		String sql = "select * from Auftrag";
-		List<Order> lp = ActiveRecordManager.getObjectList(sql, Order.class);
-		for (Order name: lp) System.out.println(name.toString());
+//		List<Order> lp = ActiveRecordManager.getObjectList(sql, Order.class);
+//		for (Order name: lp) System.out.println(name.toString());
 
 		//Get data from DB via adapter class
-		lp = DbAdapter.getOrderList();
-		for (Order name: lp) System.out.println(name.toString());
+//		lp = DbAdapter.getOrderList();
+//		for (Order name: lp) System.out.println(name.toString());
 		
 	}
 }

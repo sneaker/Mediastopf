f1837f43db87b4e1a102cc138f79e16aefbc1a03
David Tran <david.tran@hsr.ch>
2009-04-11 20:00:39 +0200

    lil bugfix

--- Code/src/ms/client/utils/ConfigHandler.java
+++ Code/src/ms/client/utils/ConfigHandler.java
@@ -7,7 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 
-import ms.server.utils.Constants;
+import ms.client.utils.Constants;
 
 
 public class ConfigHandler extends Properties {

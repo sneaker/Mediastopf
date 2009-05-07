0254d6a674e160f335e2d227d8f62d19c3505200
Martin Schwab <mschwab@hsr.ch>
2009-05-07 15:53:39 +0200

    more tests for observer

--- Code/src/ms/client/observer/DirectoryObserver.java
+++ Code/src/ms/client/observer/DirectoryObserver.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 
-import ms.client.observer.DirectoryObserverTest.MockDirectory;
-
 /**
  * Updates its subscribers whenever a filesystem change occurred in the
  * specified directory.
@@ -30,6 +28,7 @@ public class DirectoryObserver extends Observable implements Runnable {
 
 	/**
 	 * Constructor intended for test-purposes only.
+	 * 
 	 * @param dir
 	 */
 	protected DirectoryObserver(File dir) {
@@ -44,11 +43,16 @@ public class DirectoryObserver extends Observable implements Runnable {
 				Thread.sleep(POLLING_INTERVAL);
 				poll();
 			} catch (InterruptedException e) {
+			} catch (Exception e) {
+				// FIXME: Use Logger
+				System.err
+						.println("Warning: Files have been deleted from directory "
+								+ observedDirectory + ".");
 			}
 		}
 	}
 
-	public void poll() {
+	public void poll() throws Exception {
 		List<FileChange> fc = getDirectoryChanges();
 		if (fc.size() > 0) {
 			setChanged();
@@ -60,9 +64,9 @@ public class DirectoryObserver extends Observable implements Runnable {
 
 	private void takeDirectorySnapshot() {
 		lastDirectorySnapshot = observedDirectory.listFiles();
-		lastFilesizes = new ArrayList<Long>(); 
+		lastFilesizes = new ArrayList<Long>();
 		for (int i = 0; i < lastDirectorySnapshot.length; i++) {
-			lastFilesizes.add(i, lastDirectorySnapshot[i].length()); 
+			lastFilesizes.add(i, lastDirectorySnapshot[i].length());
 		}
 	}
 
@@ -73,23 +77,24 @@ public class DirectoryObserver extends Observable implements Runnable {
 		File[] newFileList = observedDirectory.listFiles();
 		ArrayList<FileChange> fc = new ArrayList<FileChange>();
 		boolean found = false;
-		
+
 		/*
-		 * 3 Fälle: 1) a b 		bisherige Datei, keine Änderung (ggf modified?) 
-		 * 			2) a ~b 	Datei gelöscht, bisher keine Aktion geplant
-		 * 			3) ~a b		Neue Datei hinzugekommen -> melden
-		 * 			4) ~a ~b	keine Änderung: kümmert uns nicht
+		 * 3 Fälle: 1) a b bisherige Datei, keine Änderung (ggf modified?) 2) a
+		 * ~b Datei gelöscht, bisher keine Aktion geplant 3) ~a b Neue Datei
+		 * hinzugekommen -> melden 4) ~a ~b keine Änderung: kümmert uns nicht
 		 * 
 		 * FIXME: Iterator benutzen (MS)
 		 */
 		for (int i = 0; i < newFileList.length; i++) {
 			found = false;
 			for (int j = 0; j < lastDirectorySnapshot.length; j++) {
-				if (lastDirectorySnapshot[j].getName().equalsIgnoreCase(newFileList[i].getName())) {
+				if (lastDirectorySnapshot[j].getName().equalsIgnoreCase(
+						newFileList[i].getName())) {
 					found = true;
 
 					if (lastFilesizes.get(j) != (newFileList[i].length())) {
-						fc.add(new FileChange(newFileList[i], FileChange.MODIFIED));
+						fc.add(new FileChange(newFileList[i],
+								FileChange.MODIFIED));
 					}
 				}
 			}
